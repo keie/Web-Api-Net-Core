7174b541c34e005395785f87504651aeffd10847
desarrollador11 <desarrollador11@softcapla.com>
2020-02-04 14:01:31 -0400

    CustomerRepository and ICustomerRepository already created

---
+++ ApiDataAccess/CustomerRepository.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace ApiDataAccess
+{
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using ApiModels;
+    using ApiRepositories;
+    using Dapper;
+
+    public class CustomerRepository:Repository<Customer>,ICustomerRepository
+    {
+        public CustomerRepository(string connectionString):base(connectionString)
+        {
+
+        }
+
+        public IEnumerable<Customer> CustomerPagedList(int page, int rows)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("@page",page);
+            parameters.Add("@rows", rows);
+            using (var connection=new SqlConnection(_connectionString))
+            {
+                return connection.Query<Customer>("dbo.CustomerPagedList",
+                    parameters,
+                    commandType:System.Data.CommandType.StoredProcedure);//ejecucionProcedure
+            }
+        }
+    }
+}
---
+++ ApiRepositories/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ApiRepositories
+{
+    using ApiModels;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public interface ICustomerRepository:IRepository<Customer>
+    {
+        IEnumerable<Customer> CustomerPagedList(int page, int rows);
+    }
+}
